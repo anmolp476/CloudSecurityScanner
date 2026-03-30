85ae226ca92aeb658a97c52c27f87521c9b4540e
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2026-03-29 22:15:04 -0400

    Removed unused useState hook

--- cloud-scanner-ui/frontend/src/pages/Login.tsx
+++ cloud-scanner-ui/frontend/src/pages/Login.tsx
@@ -35,7 +35,7 @@ const Login = () => {
     try {
       setIsLoading(true);
 
-      const response = await fetch("http://localhost:3000/scan", {
+      const response = await fetch(import.meta.env.VITE_API_URL, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
